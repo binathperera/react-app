a1c438d0f6c355bcf8d2f9bed33ce22046d711d0
Binath Perera <binathperera@bluelotus360.co>
2024-05-05 14:03:43 +0530

    changes to home page url

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/news" element={<News />} />
       </Routes>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ function Footer() {
               type="email"
               placeholder="Your Email"
             />
-            <Button buttonStyle="btn btn-primary" type="submit" url="/">
+            <Button buttonStyle="btn btn-primary" type="submit" url="/home">
               Subscribe
             </Button>
           </form>
@@ -32,34 +32,34 @@ function Footer() {
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
             <h2>About Us</h2>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Terms of Service</Link>
+            <Link to="/home">Testimonials</Link>
+            <Link to="/home">Terms of Service</Link>
           </div>
           <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Sponsorships</Link>
+            <Link to="/home">Contact</Link>
+            <Link to="/home">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div className="footer-link-items">
             <h2>Projects</h2>
-            <Link to="/">Media</Link>
-            <Link to="/">Ambassadors</Link>
+            <Link to="/home">Media</Link>
+            <Link to="/home">Ambassadors</Link>
           </div>
           <div className="footer-link-items">
             <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+            <Link to="/home">Instagram</Link>
+            <Link to="/home">Facebook</Link>
+            <Link to="/home">Youtube</Link>
+            <Link to="/home">Twitter</Link>
           </div>
         </div>
       </div>
       <section className="social-media">
         <div className="social-media-wrap">
           <div className="footer-logo">
-            <Link to="/" className="footer-logo">
+            <Link to="/home" className="footer-logo">
               <img src="images/ecsc-navbar-logo.png" />
             </Link>
           </div>
@@ -69,7 +69,7 @@ function Footer() {
           <div className="social-icons">
             <Link
               className="social-icon-link facebook"
-              to="/"
+              to="/home"
               target="_blank"
               aria-label="Facebook"
             >
@@ -77,7 +77,7 @@ function Footer() {
             </Link>
             <Link
               className="social-icon-link instagram"
-              to="/"
+              to="/home"
               target="_blank"
               aria-label="Instagram"
             >
@@ -85,7 +85,7 @@ function Footer() {
             </Link>
             <Link
               className="social-icon-link youtube"
-              to="/"
+              to="/home"
               target="_blank"
               aria-label="Youtube"
             >
@@ -93,7 +93,7 @@ function Footer() {
             </Link>
             <Link
               className="social-icon-link twitter"
-              to="/"
+              to="/home"
               target="_blank"
               aria-label="Twitter"
             >
@@ -101,7 +101,7 @@ function Footer() {
             </Link>
             <Link
               className="social-icon-link twitter"
-              to="/"
+              to="/home"
               target="_blank"
               aria-label="LinkedIn"
             >
